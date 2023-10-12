06abdfeead2fa8ffcf430e485adaf91910d5a156
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-12 15:51:25 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+package renucorejava;
+
+public class main {
+int x;
+public  main( int y) {
+	x=y;
+}
+public static void main(String args[]) {
+	main obj=new main(40);
+	System.out.println(obj.x);
+	
+}
+}
