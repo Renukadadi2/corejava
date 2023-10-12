aa08c6dbd29fdfc59d965c5566c7b908fac997c0
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-12 15:47:56 +0530

    Add files via upload

---
+++ constructors.java
@@ -0,0 +1,14 @@
+package renucorejava;
+
+public class constructors {
+int x;
+
+public constructors() {
+	x=5;
+}
+	public static void main(String[]args) {
+	constructors obj=new constructors();
+	System.out.println(obj.x);
+	}
+}
+
