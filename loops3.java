101f68c1e2ee4a013904c86f1a2bbe5d9bb1754b
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 13:58:43 +0530

    Add files via upload

---
+++ loops3.java
@@ -0,0 +1,10 @@
+class Loops{
+public static void main(String args[]){
+System.out.println("loops begin");
+for(int x=1;x<=10;x++){
+System.out.println("x="+x);
+}
+System.out.println("loops ends");
+//System.out.println("x="+x);
+}
+}
