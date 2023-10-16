3abc165daa544c0262ff75deb9793abf31bfb107
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 14:02:10 +0530

    Add files via upload

---
+++ scanner1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Input{
+public static void main(String args[]){
+Scanner obj=new Scanner(System.in);
+System.out.println("enter first number");
+int a=obj.nextInt();
+System.out.println("enter second number");
+int b=obj.nextInt();
+System.out.println("enter third number");
+if(x>y&&x>z){
+System.out.println(x);
+}
+else(y>z)
+{
+System.out.println(y);
+}
+else {
+System.out.println(z);
+}
+}
+}
