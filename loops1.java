9a0af11d2f1ccf01586c8111c0adf8d0c40b2935
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 13:58:05 +0530

    Add files via upload

---
+++ loops1.java
@@ -0,0 +1,12 @@
+class Loops{
+public static void main(String r[]){
+int x=10;
+{
+int y=20;
+System.out.println("x="+x);
+System.out.println("y="+y);
+}
+System.out.println("x="+x);
+//System.out.println("y="+y);
+}
+}
