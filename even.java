0d17a6fc3f1eab574c3c7f33630758064fdb5135
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 13:55:57 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,21 @@
+class Add{
+public static void main(String args[]){
+int a=Integer.parseInt(args[0]);
+if(a%2==0){
+System.out.println("it is an even number");
+a=a+2;
+System.out.println("next number is"+a);
+a=a+2;
+System.out.println("next number is"+a);
+a=a+2;
+System.out.println("next number is"+a);
+a=a+2;
+System.out.println("next number is"+a);
+a=a+2;
+System.out.println("next number is"+a);
+}
+else{
+System.out.println("it is an odd number"+a);
+}
+}
+}
