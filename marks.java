6d2a413ffc8451ad53e4c263dca19db4013d5202
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 13:59:00 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,16 @@
+class Marks{
+public static void main(String args[]){
+int t=Integer.parseInt(args[0]);
+int e=Integer.parseInt(args[1]);
+int h=Integer.parseInt(args[2]);
+int p=Integer.parseInt(args[3]);
+int m=Integer.parseInt(args[4]);
+int c=Integer.parseInt(args[5]);
+int total=t+e+h+p+m+c;
+ int avg=total/6;
+System.out.println("total 6 subjects"+total);
+System.out.println("avg 6 subjects"+avg);
+}
+}
+
+
