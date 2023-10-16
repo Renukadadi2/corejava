1efbc9061e48b1ba2174b2ab9b94451b0b8fa47d
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 13:56:20 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,16 @@
+package javaprograms;
+import java.util.Scanner;
+ class Index {
+public static void main(String ar[]) {
+		Scanner scan=new Scanner(System.in);
+		int marks[]=new int[10];
+		for(int i=10;i<10;i++) {
+			System.out.println("enter a number");
+			int x=scan.nextInt();
+			marks[i]=x;
+		}
+		for(int i=0;i<=10;i++) {
+			System.out.println("marks[i]");
+}
+}
+}
