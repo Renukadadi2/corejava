af59d169c870bb3a0fd3c49676301c8aa982e977
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 13:56:52 +0530

    Add files via upload

---
+++ factors.java
@@ -0,0 +1,12 @@
+class Factors{
+public static void main(String args[]){
+int n=Integer.parseInt(args[0]);
+System.out.println("enter a number");
+System.out.println(n);
+for(i=1;i<=n;i=i+1);
+if(n%1==0){
+System.out.println(i);
+}
+}
+}
+
