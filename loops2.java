e740ecb438c415ece4a53813bc8acb52ebaf6857
Renukadadi2 <147369474+Renukadadi2@users.noreply.github.com>
2023-10-16 13:58:21 +0530

    Add files via upload

---
+++ loops2.java
@@ -0,0 +1,11 @@
+class Loop{
+public static void main(String args[]){
+int x;
+System.out.println("loops begins");
+for(x=1; x<=10; x++){
+System.out.println("x="+x);
+}
+System.out.println("loops ends");
+System.out.println("x="+x);
+}
+}
